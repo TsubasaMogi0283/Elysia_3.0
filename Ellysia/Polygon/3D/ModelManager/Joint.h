27d72d0f1730d1433ae245873fe18cffdc76f232
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-04-16 11:54:06 +0900

    SkeletonとJointの構造体を作った

---
+++ Ellysia/Polygon/3D/ModelManager/Joint.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <QuaternionTransform.h>
+#include "Matrix4x4.h"
+
+#include <string>
+#include <vector>
+#include <optional>
+
+struct Joint {
+	//Transform情報
+	QuaternionTransform transform;
+	//LocalMatrix
+	Matrix4x4 localMatrix;
+	//SkeletonSpaceでの変換行列
+	Matrix4x4 skeletonSpaceMatrix;
+	//名前
+	std::string name;
+	//子JointのIndexのリスト。いなければ空。
+	std::vector<int32_t> cheldren;
+	//自信のIndex
+	int32_t index;
+	//親JointのIndex。いなければnull
+	//optional...ある型にbool値で無効か有効かの情報を追加した型
+	std::optional<int32_t>parent;
+
+};
--- Ellysia/Polygon/3D/ModelManager/ReadNode.cpp
+++ Ellysia/Polygon/3D/ModelManager/ReadNode.cpp
@@ -10,30 +10,30 @@ ReadNode* ReadNode::GetInstance(){
 Node ReadNode::Read(aiNode* node){
     Node result = {};
 
-    //nodeのlocalMatrixを取得
-    aiMatrix4x4 ailocalMatrix = node->mTransformation;
-    //列ベクトル形式を行ベクトル形式に転置
-    ailocalMatrix.Transpose();
-    //他の要素も同様に
-    result.localMatrix.m[0][0] = ailocalMatrix[0][0];
-    result.localMatrix.m[0][1] = ailocalMatrix[0][1];
-    result.localMatrix.m[0][2] = ailocalMatrix[0][2];
-    result.localMatrix.m[0][3] = ailocalMatrix[0][3];
+    ////nodeのlocalMatrixを取得
+    //aiMatrix4x4 ailocalMatrix = node->mTransformation;
+    ////列ベクトル形式を行ベクトル形式に転置
+    //ailocalMatrix.Transpose();
+    ////他の要素も同様に
+    //result.localMatrix.m[0][0] = ailocalMatrix[0][0];
+    //result.localMatrix.m[0][1] = ailocalMatrix[0][1];
+    //result.localMatrix.m[0][2] = ailocalMatrix[0][2];
+    //result.localMatrix.m[0][3] = ailocalMatrix[0][3];
  
-    result.localMatrix.m[1][0] = ailocalMatrix[1][0];
-    result.localMatrix.m[1][1] = ailocalMatrix[1][1];
-    result.localMatrix.m[1][2] = ailocalMatrix[1][2];
-    result.localMatrix.m[1][3] = ailocalMatrix[1][3];
-    
-    result.localMatrix.m[2][0] = ailocalMatrix[2][0];
-    result.localMatrix.m[2][1] = ailocalMatrix[2][1];
-    result.localMatrix.m[2][2] = ailocalMatrix[2][2];
-    result.localMatrix.m[2][3] = ailocalMatrix[2][3];
+    //result.localMatrix.m[1][0] = ailocalMatrix[1][0];
+    //result.localMatrix.m[1][1] = ailocalMatrix[1][1];
+    //result.localMatrix.m[1][2] = ailocalMatrix[1][2];
+    //result.localMatrix.m[1][3] = ailocalMatrix[1][3];
+    //
+    //result.localMatrix.m[2][0] = ailocalMatrix[2][0];
+    //result.localMatrix.m[2][1] = ailocalMatrix[2][1];
+    //result.localMatrix.m[2][2] = ailocalMatrix[2][2];
+    //result.localMatrix.m[2][3] = ailocalMatrix[2][3];
 
-    result.localMatrix.m[3][0] = ailocalMatrix[3][0];
-    result.localMatrix.m[3][1] = ailocalMatrix[3][1];
-    result.localMatrix.m[3][2] = ailocalMatrix[3][2];
-    result.localMatrix.m[3][3] = ailocalMatrix[3][3];
+    //result.localMatrix.m[3][0] = ailocalMatrix[3][0];
+    //result.localMatrix.m[3][1] = ailocalMatrix[3][1];
+    //result.localMatrix.m[3][2] = ailocalMatrix[3][2];
+    //result.localMatrix.m[3][3] = ailocalMatrix[3][3];
 
 
 
