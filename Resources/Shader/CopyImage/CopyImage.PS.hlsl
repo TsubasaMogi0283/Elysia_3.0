0008db5642f02e994f97ec0d07b7d0ded12cecf3
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-04-18 11:29:57 +0900

    PixelShaderを作った

--- Resources/Shader/CopyImage/CopyImage.PS.hlsl
+++ Resources/Shader/CopyImage/CopyImage.PS.hlsl
@@ -1,4 +1,17 @@
-float4 main() : SV_TARGET
-{
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+#include "CopyImage.hlsli"
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSample : register(s0);
+
+
+struct PixelShaderOutput{
+    float32_t4 color : SV_TARGET0;
+};
+
+//設定されたTextureの内容を出力するだけ
+PixelShaderOutput main(VertexShaderOutput input){
+    PixelShaderOutput output;
+    
+    output.color = gTexture.Sample(gSample, input.texcoord);
+    
+    return output;
 }
