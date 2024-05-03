105c9401f20734ad34ccf86561d5312c4e6e9ab3
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-05-04 05:20:12 +0900

    三角形を作るやつを持ってきて何とか表示させたい

--- Resources/Shader/CopyImage/CopyImage.PS.hlsl
+++ Resources/Shader/CopyImage/CopyImage.PS.hlsl
@@ -1,17 +1,58 @@
-#include "CopyImage.hlsli"
-Texture2D<float32_t4> gTexture : register(t0);
-SamplerState gSample : register(s0);
+//#include "CopyImage.hlsli"
+//Texture2D<float32_t4> gTexture : register(t0);
+//SamplerState gSample : register(s0);
+//
+//
+//struct PixelShaderOutput{
+//    float32_t4 color : SV_TARGET0;
+//};
+//
+////設定されたTextureの内容を出力するだけ
+//PixelShaderOutput main(VertexShaderOutput input){
+//    PixelShaderOutput output;
+//    
+//    output.color = gTexture.Sample(gSample, input.texcoord);
+//    
+//    return output;
+//}
 
 
-struct PixelShaderOutput{
+////PixelShader
+//ここからの出力は色で、OutputManagerに送られて
+//ブレンド処理も行い画面に色が打ち込まれる
+ 
+ 
+//テクスチャを張り付けたり、ライティングを行ったり出来る。
+//最も重要なShader
+
+//ラスタライザー
+//		↓VertexShadeOutput
+//ピクセルシェーダー
+//		↓PixelShadeOutput
+//	 出力結合
+
+
+
+
+//Material...色など三角形の表面の材質をけっていするもの
+struct Material
+{
+    float32_t4 color;
+};
+
+//ConstantBuffer<構造体>変数名:register(b0);
+ConstantBuffer<Material> gMaterial : register(b0);
+struct PixelShaderOutput
+{
+
     float32_t4 color : SV_TARGET0;
 };
 
-//設定されたTextureの内容を出力するだけ
-PixelShaderOutput main(VertexShaderOutput input){
+
+PixelShaderOutput main()
+{
     PixelShaderOutput output;
-    
-    output.color = gTexture.Sample(gSample, input.texcoord);
-    
+    output.color = gMaterial.color;
+
     return output;
 }
