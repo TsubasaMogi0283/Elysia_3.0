301820a4faac9eb6eeca0700ccbc6cc109e119ba
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-05-04 05:12:50 +0900

    一旦三角形が描画出来るか確認する

--- Resources/Shader/CopyImage/CopyImage.VS.hlsl
+++ Resources/Shader/CopyImage/CopyImage.VS.hlsl
@@ -1,32 +1,50 @@
 #include "CopyImage.hlsli"
 
-//三角形で覆われればOK
-static const uint32_t NUM_VERTEX = 3;
-static const float32_t4 POSITIONS[NUM_VERTEX] ={
-    //左上
-    { -1.0f, 1.0f, 0.0f, 1.0f },
-    //右上
-    { 3.0f, 1.0f, 0.0f, 1.0f },
-    //左下
-    { -1.0f, -3.0f, 0.0f, 1.0f },
-    
+////三角形で覆われればOK
+//static const uint32_t NUM_VERTEX = 3;
+//static const float32_t4 POSITIONS[NUM_VERTEX] ={
+//    //左上
+//    { -1.0f, 1.0f, 0.0f, 1.0f },
+//    //右上
+//    { 3.0f, 1.0f, 0.0f, 1.0f },
+//    //左下
+//    { -1.0f, -3.0f, 0.0f, 1.0f },
+//    
+//};
+//
+//
+//
+//
+//static const float32_t2 TEXCOORDS[NUM_VERTEX] ={
+//    //左上
+//    { 0.0f, 0.0f },
+//    //右上
+//    { 2.0f, 0.0f },
+//    //左下
+//    { 0.0f, 2.0f },
+//};
+//
+//VertexShaderOutput main(uint32_t vertexId : SV_VertexID){
+//    VertexShaderOutput output;
+//    output.position = POSITIONS[vertexId];
+//    output.texcoord = TEXCOORDS[vertexId];
+//    return output;
+//}
+
+
+struct VertexShaderOutput
+{
+    float32_t4 position : SV_POSITION;
 };
 
-
-
-
-static const float32_t2 TEXCOORDS[NUM_VERTEX] ={
-    //左上
-    { 0.0f, 0.0f },
-    //右上
-    { 2.0f, 0.0f },
-    //左下
-    { 0.0f, 2.0f },
+struct VertexShaderInput
+{
+    float32_t4 position : POSITION0;
 };
 
-VertexShaderOutput main(uint32_t vertexId : SV_VertexID){
+VertexShaderOutput main(VertexShaderInput input)
+{
     VertexShaderOutput output;
-    output.position = POSITIONS[vertexId];
-    output.texcoord = TEXCOORDS[vertexId];
+    output.position = input.position;
     return output;
 }
