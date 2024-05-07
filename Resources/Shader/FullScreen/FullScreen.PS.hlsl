ec516ee200b1a29d94a37b10d305a28d349c8924
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-05-08 00:47:46 +0900

    セピア調をメモった。 フラグで切り替え出来るようにしたいね。

--- Resources/Shader/FullScreen/FullScreen.PS.hlsl
+++ Resources/Shader/FullScreen/FullScreen.PS.hlsl
@@ -22,7 +22,11 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     output.color = gTexture.Sample(gSample, input.texcoord);
     float32_t value = dot(output.color.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    
     output.color.rgb = float32_t3(value, value, value);
+    //セピア
+    //output.color.rgb = value*float32_t3(1.0f,74.0f/107.0f,43.0f/107.0f);
+    
     
     return output;
 }
