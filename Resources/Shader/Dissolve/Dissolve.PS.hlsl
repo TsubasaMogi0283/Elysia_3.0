ce7bac3e746af7e0dd9f55a0d352c97797e02eda
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-06-11 17:12:50 +0900

    Edge付きだとこんなものなのかな

--- Resources/Shader/Dissolve/Dissolve.PS.hlsl
+++ Resources/Shader/Dissolve/Dissolve.PS.hlsl
@@ -25,13 +25,19 @@ PixelShaderOutput main(VertexShaderOutput input){
     
     float mask = gMaskTexture.Sample(gSample, input.texcoord);
     //maskの値が設定したtherashold以下の場合はdiscard
+    
     if (mask <= gDissolve.therashold)
     {
         discard;
         
     }
     
+    float edge = 1.0f - smoothstep(0.5f, 0.53f, mask);
     output.color = gTexture.Sample(gSample, input.texcoord);
+    
+    
+    //Edgeっぽいほど指定した色を加算
+    output.color.rgb += edge * float3(1.0f, 1.0f, 1.0f);
     
     return output;
 }
